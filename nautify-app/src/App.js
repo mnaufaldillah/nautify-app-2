e2c2264a58d024541a87785d30f0b0a2512ea1a8
mnaufaldillah <mnaufaldillah@gmail.com>
2022-04-18 21:48:47 +0700

    Dropdown, Detail, and Listbox

--- nautify-app/src/App.js
+++ nautify-app/src/App.js
@@ -1,23 +1,122 @@
 import logo from './logo.svg';
 import './App.css';
+import Dropwdown from './Components/Dropdown';
+import Listbox from './Components/Listbox';
+import Detail from './Components/Detail';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Credentials } from './Credrntials';
 
-function App() {
+
+const App = () => {
+  const spotify = Credentials();
+
+  const data = [
+    {value: 1, name: 'A'},
+    {value: 2, name: 'B'},
+    {value: 3, name: 'C'}
+  ]
+
+  const [token, setToken] = useState('');
+  const [genres, setGenres] = useState({selectedGenre: '', listOfGenresFromAPI: []});
+  const [playlist, setPlaylist] = useState({selectedPlaylist: '', listOfPlaylistFromAPI: []});
+  const [tracks, setTracks] = useState({selectedTrack: '', listOfTracksFromAPI: []});
+  const [trackDetail, setTrackDetail] = useState(null);
+  
+
+  useEffect(() => {
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      setToken(tokenResponse.data.access_token);
+
+      axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+      })
+      .then (genreResponse => {        
+        setGenres({
+          selectedGenre: genres.selectedGenre,
+          listOfGenresFromAPI: genreResponse.data.categories.items
+        })
+      });
+      
+    });
+
+  }, [genres.selectedGenre, spotify.ClientId, spotify.ClientSecret]);
+
+  const genreChanged = (val) => {
+    setGenres({
+      selectedGenre: val, 
+      listOfGenresFromAPI: genres.listOfGenresFromAPI
+    });
+
+    axios(`https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`, {
+      method: 'GET',
+      headers: { 'Authorization' : 'Bearer ' + token}
+    })
+    .then(playlistResponse => {
+      setPlaylist({
+        selectedPlaylist: playlist.selectedPlaylist,
+        listOfPlaylistFromAPI: playlistResponse.data.playlists.items
+      })
+    });
+
+    console.log(val);
+  }
+
+  const playlistChanged = (val) => {
+    console.log(val);
+    setPlaylist({
+      selectedPlaylist: val,
+      listOfPlaylistFromAPI: playlist.listOfPlaylistFromAPI
+    });
+  }
+
+  const buttonClicked = (e) => {
+    e.preventDefault();
+
+    axios(`https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`, {
+      method: 'GET',
+      headers: {
+        'Authorization' : 'Bearer ' + token
+      }
+    })
+    .then(tracksResponse => {
+      setTracks({
+        selectedTrack: tracks.selectedTrack,
+        listOfTracksFromAPI: tracksResponse.data.items
+      })
+    });
+  }
+
+  const listboxClicked = (val) => {
+    const currentTracks = [...tracks.listOfTracksFromAPI];
+    const trackInfo = currentTracks.filter(t => t.track.id === val);
+    setTrackDetail(trackInfo[0].track);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={buttonClicked}>
+        <Dropwdown label="Genre :" options={genres.listOfGenresFromAPI} selectedValue={genres.selectedGenre} changed={genreChanged} />
+        <Dropwdown label="Playlist :" options={playlist.listOfPlaylistFromAPI} selectedValue={playlist.selectedPlaylist} changed={playlistChanged} />
+        <div className="col-sm-6 row form-group px-0">
+          <button type='submit' className="btn btn-success col-sm-12">
+            Search
+          </button>
+        </div>
+      </form>
+      <div className='row'>
+        <Listbox items={tracks.listOfTracksFromAPI} clicked={listboxClicked} />
+        {trackDetail && <Detail {...trackDetail} /> }
+      </div>
     </div>
   );
 }
